3914613aaf9937b15c36a2e2caac574648ec8c5f
Mschkin <christianbild1@web.de>
2020-12-09 03:22:11 +0100

    forgot

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ use std::time:: Instant;
 
 
 fn main() {
-    
+    let a = ['A', 'B', 'C'];
+    let b = [1, 4];
+    let c = [true, false];
+    let d = ['x', 'y'];
+
+    for (a, b, c, d) in iproduct!(&a, &b, &c, &d) {
+        //println!("{} {} {} {}", a, b, c, d);
+    }
 
     let start = Instant::now();
 
     let mut sat_maker = tents::SatMaker::new("src/tents.txt");
     let mut duration = start.elapsed();
-    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    //println!("Time elapsed in expensive_function() is: {:?}", duration);
     sat_maker.solve_sat();
     duration = start.elapsed();
-    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    //println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
 
