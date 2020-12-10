dd94747bb3e2c57bbb5c73e50db3d7281e2ff760
Mschkin <christianbild1@web.de>
2020-12-10 04:01:25 +0100

    really fast now

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
 mod tents;
 use std::time:: Instant;
 
-#[macro_use]
-extern crate itertools;
+
+
 
 fn main() {
-    let a = ['A', 'B', 'C'];
-    let b = [1, 4];
-    let c = [true, false];
-    let d = ['x', 'y'];
-
-    for (a, b, c, d) in iproduct!(&a, &b, &c, &d) {
-        //println!("{} {} {} {}", a, b, c, d);
-    }
-
     let start = Instant::now();
 
     let mut sat_maker = tents::SatMaker::new("src/tents.txt");
     let mut duration = start.elapsed();
-    //println!("Time elapsed in expensive_function() is: {:?}", duration);
+    println!("encoding time: {:?}", duration);
     sat_maker.solve_sat();
-    duration = start.elapsed();
-    //println!("Time elapsed in expensive_function() is: {:?}", duration);
+    let duration1 = start.elapsed();
+    println!("solving time: {:?}", duration1-duration);
 }
 
