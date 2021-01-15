24cd4d12465cc1833031b31552312617915b1862
Mingzi <mingzi.kong1990@gmail.com>
2021-01-15 14:23:28 +0100

    Update tents.rs

--- src/tents.rs
+++ src/tents.rs
@@ -602,8 +602,8 @@ impl SatMaker {
             println!("UNSAT")
         } else {
             println!("Tentmap:#~#{:?}#~#", tent_pos);
+            println!("{}",sol_content);
         }
-        println!("{}",sol_content);
         //self.find_unsat_clause();
     }
 
